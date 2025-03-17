9dc75ddc3f7639258563b9048a6e70bb0b2d5547
nguyentrongvanviet <nguyentrongvanviet@gmail.com>
2025-03-17 22:15:03 +0700

    okay

--- build/include/raylib/cmake_install.cmake
+++ build/include/raylib/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/chanh/OneDrive/Desktop/Project/CS163/Data-Visualizer/include/raylib
+# Install script for directory: V:/Data-Visualizer/include/raylib
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,12 +34,12 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/chanh/OneDrive/Desktop/Project/CS163/Data-Visualizer/build/include/raylib/raylib/cmake_install.cmake")
+  include("V:/Data-Visualizer/build/include/raylib/raylib/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/chanh/OneDrive/Desktop/Project/CS163/Data-Visualizer/build/include/raylib/install_local_manifest.txt"
+  file(WRITE "V:/Data-Visualizer/build/include/raylib/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
