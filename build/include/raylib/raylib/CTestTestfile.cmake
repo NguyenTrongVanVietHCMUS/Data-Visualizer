aafe64dd60c47fb7cb7841582656fab2b4111d76
ChubeeCode <chanhchuong0702@gmail.com>
2025-04-08 22:33:56 +0700

    Commit

--- build/include/raylib/raylib/CTestTestfile.cmake
+++ build/include/raylib/raylib/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: V:/Data-Visualizer/include/raylib/src
-# Build directory: V:/Data-Visualizer/build/include/raylib/raylib
+# Source directory: C:/Users/chanh/OneDrive/Desktop/Project/CS163/Chuong/Data-Visualizer/include/raylib/src
+# Build directory: C:/Users/chanh/OneDrive/Desktop/Project/CS163/Chuong/Data-Visualizer/build/include/raylib/raylib
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
